3b402bef61e02f73ec947444a9a6264156c77c13
dbarsam <dbarsam@gmail.com>
2012-11-23 10:36:13 -0500

    Added initial powershell profile

---
+++ profile.ps1
@@ -0,0 +1,62 @@
+## ===================================================================
+##                    Windows PowerShell Profile
+## ===================================================================
+## File:           profile.ps1
+## Purpose:        Default profile for Microsoft PowerShell
+## Author:         David Barsam
+## URL:            https://github.com/dbarsam/.ps1
+## ===================================================================
+
+## ===================================================================
+## Security Settings
+Set-ExecutionPolicy RemoteSigned
+
+## ===================================================================
+## Environment Variables
+$ProfileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$ScriptDir  = "$ProfileDir\scripts"
+$ModuleDir  = "$ProfileDir\modules"
+
+## ===================================================================
+## PS Drives
+New-PSdrive -name Scripts -PSprovider filesystem -root $ScriptDir
+New-PSdrive -name Modules -PSprovider filesystem -root $ModuleDir
+
+## ===================================================================
+## Quick Functions
+function global:Append-Path([string] $path) 
+{
+    if ( -not [string]::IsNullOrEmpty($path) -and (test-path $path) -and (-not $env:PATH.contains($path)) ) 
+    {
+        $env:PATH += ';' + $path
+    }
+}
+
+function global:Export-Base64([string] $path)
+{
+    return [convert]::ToBase64String((get-content $path -encoding byte))
+}
+
+function global:Get-Path() 
+{
+    return $ENV:PATH 
+}
+
+function global:Get-PathList()
+{
+    return $ENV:Path.Replace('"', '').Split(';') | Sort-Object -unique
+}
+
+function global:Start-Explorer([string] $loc = '.')
+{
+    explorer "/e,"$loc"" 
+}
+
+## ===================================================================
+## Append the Path Environment
+Append-Path "$ScriptDir"
+Append-Path "$ModuleDir"
+
+## ===================================================================
+## Alias
+set-alias ex Start-Explorer
